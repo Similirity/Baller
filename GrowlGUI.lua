05339aee3948610be3cbd1a4924dcebc0c0921c4
Similirity <84045603+Similirity@users.noreply.github.com>
2022-10-23 20:35:32 +0100

    Growl GUI thing

---
+++ GrowlGUI.lua
@@ -0,0 +1,33 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local ImageLabel = Instance.new("ImageLabel")
+
+ScreenGui.Parent = game.Workspace.Growl.RushNew
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(99, 15, 26)
+Frame.Size = UDim2.new(0, 1194, 0, 631)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(0, 1188, 0, 631)
+TextLabel.Font = Enum.Font.Unknown
+TextLabel.Text = "YOUR FAULT YOUR FAULT YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT YOUR FAULT YOUR FAULT YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT YOUR FAULT YOUR FAULT YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT  YOUR FAULT"
+TextLabel.TextColor3 = Color3.fromRGB(107, 0, 53)
+TextLabel.TextSize = 56.000
+TextLabel.TextStrokeColor3 = Color3.fromRGB(122, 0, 61)
+TextLabel.TextStrokeTransparency = 0.000
+TextLabel.TextWrapped = true
+TextLabel.TextYAlignment = Enum.TextYAlignment.Top
+
+ImageLabel.Parent = Frame
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.Size = UDim2.new(0, 1194, 0, 631)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=2270890360"
+ImageLabel.ImageColor3 = Color3.fromRGB(255, 0, 128)
+ImageLabel.ImageTransparency = 0.800
+ImageLabel.ScaleType = Enum.ScaleType.Crop
