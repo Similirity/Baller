4d8512bb1decc9375e287bedf612de7fd6556398
Similirity <84045603+Similirity@users.noreply.github.com>
2022-10-24 11:22:09 +0100

    gui to lua is goated fr bro

---
+++ BallerGUI.lua
@@ -0,0 +1,104 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local ImageLabel = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local ImageLabel_2 = Instance.new("ImageLabel")
+local ImageLabel_3 = Instance.new("ImageLabel")
+local ImageLabel_4 = Instance.new("ImageLabel")
+local ImageLabel_5 = Instance.new("ImageLabel")
+local ImageLabel_6 = Instance.new("ImageLabel")
+local ImageLabel_7 = Instance.new("ImageLabel")
+local ImageLabel_8 = Instance.new("ImageLabel")
+local ImageLabel_9 = Instance.new("ImageLabel")
+
+ScreenGui.Parent = game.Workspace.Baller.RushNew
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(108, 0, 0)
+Frame.BackgroundTransparency = 1.000
+Frame.Size = UDim2.new(1, 0, 1, 0)
+
+ImageLabel.Parent = Frame
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=11151804223"
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderSizePixel = 0
+TextLabel.Size = UDim2.new(1, 0, 0.191759109, 0)
+TextLabel.Font = Enum.Font.Unknown
+TextLabel.Text = "BALLER"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextStrokeTransparency = 0.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0, 0, 0.808240891, 0)
+TextLabel_2.Size = UDim2.new(1, 0, 0.191759109, 0)
+TextLabel_2.Font = Enum.Font.Unknown
+TextLabel_2.Text = "BALLER"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextStrokeTransparency = 0.000
+TextLabel_2.TextWrapped = true
+
+ImageLabel_2.Parent = Frame
+ImageLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_2.Position = UDim2.new(0, 0, 0.665610135, 0)
+ImageLabel_2.Size = UDim2.new(0.195979893, 0, 0.334389865, 0)
+ImageLabel_2.Image = "http://www.roblox.com/asset/?id=11339922083"
+
+ImageLabel_3.Parent = Frame
+ImageLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_3.Size = UDim2.new(0.195979893, 0, 0.334389865, 0)
+ImageLabel_3.Image = "http://www.roblox.com/asset/?id=11339922083"
+
+ImageLabel_4.Parent = Frame
+ImageLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_4.Position = UDim2.new(0.804020107, 0, 0, 0)
+ImageLabel_4.Size = UDim2.new(0.195979893, 0, 0.334389865, 0)
+ImageLabel_4.Image = "http://www.roblox.com/asset/?id=11339922083"
+
+ImageLabel_5.Parent = Frame
+ImageLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_5.Position = UDim2.new(0.804020107, 0, 0.665610135, 0)
+ImageLabel_5.Size = UDim2.new(0.195979893, 0, 0.334389865, 0)
+ImageLabel_5.Image = "http://www.roblox.com/asset/?id=11339922083"
+
+ImageLabel_6.Parent = Frame
+ImageLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_6.BackgroundTransparency = 1.000
+ImageLabel_6.Position = UDim2.new(-0.0460636318, 0, 0.169572115, 0)
+ImageLabel_6.Size = UDim2.new(0.504187584, 0, 0.52139461, 0)
+ImageLabel_6.Image = "http://www.roblox.com/asset/?id=11299955571"
+
+ImageLabel_7.Parent = Frame
+ImageLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_7.BackgroundTransparency = 1.000
+ImageLabel_7.Position = UDim2.new(0.519263029, 0, 0.497622818, 0)
+ImageLabel_7.Size = UDim2.new(0.504187584, 0, 0.52139461, 0)
+ImageLabel_7.Image = "http://www.roblox.com/asset/?id=11299955571"
+
+ImageLabel_8.Parent = Frame
+ImageLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_8.BackgroundTransparency = 1.000
+ImageLabel_8.Position = UDim2.new(0.632328391, 0, -0.312202871, 0)
+ImageLabel_8.Size = UDim2.new(0.504187584, 0, 0.52139461, 0)
+ImageLabel_8.Image = "http://www.roblox.com/asset/?id=11299955571"
+
+ImageLabel_9.Parent = Frame
+ImageLabel_9.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_9.BackgroundTransparency = 1.000
+ImageLabel_9.Position = UDim2.new(-0.206030056, 0, 0.849445283, 0)
+ImageLabel_9.Size = UDim2.new(0.504187584, 0, 0.52139461, 0)
+ImageLabel_9.Image = "http://www.roblox.com/asset/?id=11299955571"
